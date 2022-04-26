da8b228af83f5c142046591f7c09867df302176e
Sergii Zlishchev <47575515+DarthMarr13@users.noreply.github.com>
2022-04-26 14:27:03 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const x = prompt ('Enter first number');
+const y = prompt ('Enter second number');
+console.log (`Calculations are finished!
+Sum: ${x} + ${y} = ${+x + +y}
+Diff: ${x} - ${y} = ${+x - +y}
+Mult: ${x} * ${y} = ${+x * +y}
+Div: ${x} / ${y} = ${+x / +y}`)
+alert (`Calculations are finished!
+Sum: ${x} + ${y} = ${+x + +y}
+Diff: ${x} - ${y} = ${+x - +y}
+Mult: ${x} * ${y} = ${+x * +y}
+Div: ${x} / ${y} = ${+x / +y}`)
